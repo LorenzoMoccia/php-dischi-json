f82860ed97f1470cf7ac3a73ba6456b38739d5e7
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-12-16 16:45:01 +0100

    Push

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+
+createApp({
+  data() {
+    return {
+      dischi: [],
+    }
+  },
+  created(){
+    axios.get('http://localhost/php-dischi-json/database.php').then((res) => {
+        this.dischi = res.data;
+    });
+    
+  }
+}).mount('#app')
